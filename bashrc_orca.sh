361e92099a6fa746bbcf43b8cd4d7db156cfb5a6
Xin Shi <xshi@orca4>
2014-02-27 15:08:25 -0500

    add rcac prompt

--- bashrc_orca.sh
+++ bashrc_orca.sh
@@ -347,6 +347,7 @@ sl() {
     if [ -z "$1" ]; then
 	echo "[bohr]    bohr.physics.purdue.edu"
 	echo "[clyde]   clyde.physics.purdue.edu"
+	echo "[rcac]    cms.rcac.purdue.edu"
 	echo "[c]       lxplus.cern.ch"
 	echo "[c5]      lxplus5.cern.ch"
 	echo "[f5]      cmslpc-sl5.fnal.gov"

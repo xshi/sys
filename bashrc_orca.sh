4e925a9f369f7516df073fcd7c60353729aceedb
Xin Shi <xshi@orca4>
2014-02-27 11:29:59 -0500

    add rcac

--- bashrc_orca.sh
+++ bashrc_orca.sh
@@ -368,6 +368,9 @@ sl() {
 	
 	clyde) ssh -Y shi210@clyde.physics.purdue.edu 
 	    ;;
+
+	rcac) ssh -Y shi210@cms.rcac.purdue.edu 
+	    ;;
 	
 	c) ssh -Y xshi@lxplus.cern.ch
 	    ;;
@@ -470,4 +473,13 @@ function tabname {
 function winname {
   printf "\e]2;$1\a"
 }
+
+
+##
+# Your previous /Users/xshi/.bash_profile file was backed up as /Users/xshi/.bash_profile.macports-saved_2014-02-18_at_07:36:36
+##
+
+# MacPorts Installer addition on 2014-02-18_at_07:36:36: adding an appropriate PATH variable for use with MacPorts.
+export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+# Finished adapting your PATH environment variable for use with MacPorts.
 

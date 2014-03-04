85fac10643a1540589d07ebf9613e462b295e300
Xin Shi <shi210@purdue.edu>
2014-03-04 13:35:48 -0500

    simplify the probmpt

--- bashrc_rcac.sh
+++ bashrc_rcac.sh
@@ -1,58 +1,67 @@
 #!/bin/sh
 
 
-export HOST=$(hostname | cut -d. -f1)
-export USER=${USER:-$(whoami)}
-
+#--------------------------------------------------
+# Prompt Setting
+#--------------------------------------------------
+HOST=$(hostname | cut -d. -f1)
 if [ "$PS1" ]; then
-  # Set auto_resume if you want TWENEX style behaviour for command names.
-  auto_resume=
-
-  # Set notify if you want to be asynchronously notified about background
-  # job completion.
-  notify=
-
-  # Make it so that failed `exec' commands do not flush this shell.
-  no_exit_on_failed_exec=
-
-  #export HOST=$(hostname | cut -d. -f1)
-  #export USER=${USER:-$(whoami)}
-  export EDITOR=vim
-  #export MAIL=/usr/spool/mail/$USER
-
-
-  #if [ ! "$LOGIN_SHELL" ]; then
-  PS1="\u_$HOST> "
-  #fi
-
-  HISTSIZE=256
-  MAILCHECK=60
-
-  xtitle() {
-    if [ $# -gt 1 ]; then
-      echo -e "\033]1;$1\a\c"
-      echo -e "\033]2;$2\a\c"
-    else
-      echo -e "\033]0;$1\a\c"
-    fi
-  }
-
-  cd_xtitle() {
-    xtitle "$USER@$HOST" "$HOST $PWD"
-  }
-
-  [ "$TERM" = "xterm" ] && PROMPT_COMMAND=cd_xtitle
-  [ -n "$DISPLAY"     ] && export GKS3Dconid=$DISPLAY
-
-  pwd() {
-    if [ "${nolinks+set}" = "set" ] ; then
-      builtin pwd
-    else
-      echo "$PWD"
-    fi
-  }
-
+  PS1="\u_$HOST$ "
+  ignoreeof=1
 fi
+
+# export HOST=$(hostname | cut -d. -f1)
+# export USER=${USER:-$(whoami)}
+
+# if [ "$PS1" ]; then
+#   # Set auto_resume if you want TWENEX style behaviour for command names.
+#   auto_resume=
+
+#   # Set notify if you want to be asynchronously notified about background
+#   # job completion.
+#   notify=
+
+#   # Make it so that failed `exec' commands do not flush this shell.
+#   no_exit_on_failed_exec=
+
+#   #export HOST=$(hostname | cut -d. -f1)
+#   #export USER=${USER:-$(whoami)}
+#   export EDITOR=vim
+#   #export MAIL=/usr/spool/mail/$USER
+
+
+#   #if [ ! "$LOGIN_SHELL" ]; then
+#   PS1="\u_$HOST> "
+#   #fi
+
+#   HISTSIZE=256
+#   MAILCHECK=60
+
+#   xtitle() {
+#     if [ $# -gt 1 ]; then
+#       echo -e "\033]1;$1\a\c"
+#       echo -e "\033]2;$2\a\c"
+#     else
+#       echo -e "\033]0;$1\a\c"
+#     fi
+#   }
+
+#   cd_xtitle() {
+#     xtitle "$USER@$HOST" "$HOST $PWD"
+#   }
+
+#   [ "$TERM" = "xterm" ] && PROMPT_COMMAND=cd_xtitle
+#   [ -n "$DISPLAY"     ] && export GKS3Dconid=$DISPLAY
+
+#   pwd() {
+#     if [ "${nolinks+set}" = "set" ] ; then
+#       builtin pwd
+#     else
+#       echo "$PWD"
+#     fi
+#   }
+
+# fi
 
 
 #--------------------------------------------------
@@ -79,10 +88,14 @@ export LESS='-R'
 export LESSOPEN='|~/.sys/lessfilter.sh %s' 
 unset SSH_ASKPASS  
 
-# settings for CMS 
+
+#--------------------------------------------------
+# Settings for CMS 
+#--------------------------------------------------
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 source /grp/cms/tools/glite/setup.sh 
 source /grp/cms/crab/crab.sh 
+
 
 #--------------------------------------------------
 # Functions 

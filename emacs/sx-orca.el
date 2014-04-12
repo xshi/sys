d24710bf228b06d92c5887fe7b8f7cfe398c326c
Xin Shi <xshi@orca4>
2014-04-12 10:21:45 -0400

    Add chinese font fix

--- emacs/sx-orca.el
+++ emacs/sx-orca.el
@@ -36,3 +36,31 @@
 ; Aspell 
 ;-----------------------------------------------------------
 (setq ispell-program-name "aspell")
+
+;-----------------------------------------------------------
+; tramp fix 
+;-----------------------------------------------------------
+
+;; (tramp-compat-funcall
+;;  'buffer-substring-no-properties
+;;  (point-min)
+;;  (min (+ (point-min) tramp-echo-mark-marker-length) (point-max)))
+
+;-----------------------------------------------------------
+; Chinese font 
+;-----------------------------------------------------------
+
+(when (display-graphic-p) 
+  (setq fonts 
+        (cond ((eq system-type 'darwin)     '("Monaco"     "STHeiti")) 
+              ((eq system-type 'gnu/linux)  '("Menlo"     "WenQuanYi Zen Hei")) 
+              ((eq system-type 'windows-nt) '("Consolas"  "Microsoft Yahei")))) 
+  
+  (setq face-font-rescale-alist '(("STHeiti" . 1.2)
+				  ("Microsoft Yahei" . 1.2) 
+				  ("WenQuanYi Zen Hei" . 1.2))) 
+  (set-face-attribute 'default nil :font 
+                      (format "%s:pixelsize=%d" (car fonts) 14)) 
+  (dolist (charset '(kana han symbol cjk-misc bopomofo)) 
+    (set-fontset-font (frame-parameter nil 'font) charset 
+                      (font-spec :family (car (cdr fonts)))))) 
